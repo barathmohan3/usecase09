444f4d7e8da073a8b7c70aa1f2b1f3c057de13bc
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-12 16:52:37 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,11 @@
 variable "cluster_name" {
- description = "Name of the EKS cluster"
  type = string
 }
 
 variable "subnets" {
- description = "List of subnet IDs for the EKS cluster" 
  type = list(string)
 }
 
 variable "vpc_id" {
- description = "VPC ID where the EKS cluster will be deployed"
 type = string
 }
