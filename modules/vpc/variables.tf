7bc98984814e74c9e63513139b497a3c79d1f924
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-12 15:07:35 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,10 +1,11 @@
 variable "cidr_block" {
+  description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Optional default
+  default     = "10.0.0.0/16"
 }
 
 variable "name" {
-  description = "k8_vpc"
+  description = "Name prefix for the VPC"
   type        = string
-  default     = "myapp"  
+  default     = "myapp"
 }
