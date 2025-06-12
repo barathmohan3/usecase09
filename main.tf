75f444e624c35f005885cead8ec7c672078a52be
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-12 18:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,27 +12,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 module "vpc" {
-  source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
-  name       = "eks"
+  source     = "./modules/vpc"
+  cidr_block = "10.0.0.0/16"
+  name       = "eks"
 }
 
 module "eks" {
-  source          = "./modules/eks"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.33"
-  subnet_ids      = module.vpc.public_subnets   # ✅ Use output from VPC module
-  vpc_id          = module.vpc.vpc_id           # ✅ Use output from VPC module
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.13.0"
 
-  manage_aws_auth = true
+  cluster_name    = var.cluster_name
+  cluster_version = "1.33"
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  eks_managed_node_groups = {
-    eks_nodes = {
-      desired_size   = 2
-      max_size       = 3
-      min_size       = 1
-      instance_types = ["t3.medium"]
-    }
-  }
+  manage_aws_auth = true
+
+  eks_managed_node_groups = {
+    eks_nodes = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
 }
+
