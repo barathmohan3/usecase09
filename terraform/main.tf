54879c2ca0f36c1f84ee3829156f76532a5775d6
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-12 12:16:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.3.0"
-  backend "s3" {
-    bucket         = "my-tf-state-bucket"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
